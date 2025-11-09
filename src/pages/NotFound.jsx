77b72810aa34427bcc02404f0d9853136bdff8cd
Manu-Madhu <manum66466@gmail.com>
2025-11-10 05:20:25 +0530

    update page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,21 @@
 import { Button } from "react-bootstrap";
-
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const router = useNavigate();
   return (
-    <div className="container">
+    <main className="common-container d-flex flex-column align-items-center justify-content-center h-100 w-100">
       <h2>Page Not Found</h2>
-      <Button as="a" variant="primary">
-        Button as link
+      <Button
+        className="custom-button px-5 border-0 text-white fw-semibold rounded-0 fs-5 mt-3"
+        style={{
+          backgroundColor: "#3C3C3C",
+        }}
+        onClick={() => router("/")}
+      >
+        Home
       </Button>
-    </div>
+    </main>
   );
 };
 

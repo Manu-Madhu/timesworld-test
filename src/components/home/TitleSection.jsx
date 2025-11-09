eaedefdc6f5c752ce534b860d6ed2930d7c53ce6
Manu-Madhu <manum66466@gmail.com>
2025-11-10 05:05:16 +0530

    Fixed alignment issues in the title section.

--- src/components/home/TitleSection.jsx
+++ src/components/home/TitleSection.jsx
@@ -1,22 +1,14 @@
 const TitleSection = () => {
   return (
-    <>
-      <main className="common-container position-relative px-4 px-md-3 relative d-none d-md-flex flex-column flex-md-row align-items-center justify-justify-content-center  gap-md-4 overflow-hidden">
-        <hr className="horizontal-line-right opacity-100 position-absolute top-0" />
-        <h2 className="fw-bold welcome-text m-0 p-0 w-100 text-center">
-          WELCOME
-        </h2>
-        <hr className="horizontal-line-left opacity-100 position-absolute bottom-0 end-0" />
+      <main className="common-container position-relative px-4 px-md-3 relative d-flex flex-column flex-md-row gap-md-4 overflow-hidden">
+        <div className=" w-100 d-flex">
+          <hr className="horizontal-line opacity-100 text-end " />
+        </div>
+        <h2 className="fw-bold welcome-text m-0 p-0 text-center">WELCOME</h2>
+        <div className=" w-100 d-flex align-items-md-end">
+          <hr className="horizontal-line opacity-100 " />
+        </div>
       </main>
-
-      <main className="common-container position-relative px-4 px-md-3 relative d-flex  d-md-none d-flex flex-column flex-md-row align-items-center justify-justify-content-center  gap-md-4 overflow-hidden">
-        <hr className="horizontal-line opacity-100  w-100" />
-        <h2 className="fw-bold welcome-text m-0 p-0 w-100 text-center">
-          WELCOME
-        </h2>
-        <hr className="horizontal-line opacity-100 w-100" />
-      </main>
-    </>
   );
 };
 
